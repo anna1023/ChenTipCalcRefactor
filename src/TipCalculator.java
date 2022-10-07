f2fbc861a1462af313a9cdd4eceb1e96706c183e
BT_3W2_06 <annac101@nycstudents.net>
2022-10-07 09:28:57 -0400

    1

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class TipCalculator {
+public class TipCalculator {
+    private int numPeople;
+    private int tipPercentage;
+    private double totalBillBeforeTip;
+
+    public TipCalculator(int numPeople, int tipPercentage) {
+        this.numPeople = numPeople;
+        this.tipPercentage = tipPercentage;
+        totalBillBeforeTip = 0.0;
+    }
+
+    public double getTotalBillBeforeTip() {
+        return totalBillBeforeTip;
+    }
+    public int getTipPercentage() {
+        return tipPercentage;
+    }
+    public void addMeal(double cost){
+      totalBillBeforeTip = totalBillBeforeTip+cost;
+    }
+    public double tipAmount(){
+        return (tipPercentage/100)*totalBillBeforeTip;
+    }
+    public double totalBill (){
+        return tipAmount()+totalBillBeforeTip;
+    }
+    public double perPersonCostBeforeTip (){
+       return totalBillBeforeTip/numPeople;
+    }
+    public double perPersonTipAmount (){
+        return tipAmount()/numPeople;
+    }
+    public double perPersonTotalCost(){
+        return totalBill()/numPeople;
+    }
 }
--- src/TipCalculatorRunner.java
+++ src/TipCalculatorRunner.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class TipCalculatorRunner {
+import java.util.Scanner;
+public class TipCalculatorRunner {
+    public static void main (String[] args){
+        System.out.print("Welcome to Tip Calculator!");
+        Scanner scan = new Scanner (System.in);
+        System.out.println("How many people are in your group?");
+        int numPeople = scan.nextInt();
+        System.out.println("How much would you like to tip? (0-100)");
+        int tipPercentage = scan.nextInt();
+        TipCalculator tip = new TipCalculator(numPeople,tipPercentage);
+        double addMeal = 0;
+        while (addMeal!=-1) {
+            System.out.println("Enter your food in dollars and cents.");
+            addMeal = scan.nextDouble();
+        }
+
+        System.out.println("Total Bill Before Tip: "+tip.getTotalBillBeforeTip());
+        System.out.println("Total Percentage: "+tip.getTipPercentage());
+        System.out.println("Total Tip: "+tip.tipAmount());
+
+
+    }
 }
